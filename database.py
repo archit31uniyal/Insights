cc2c502a80ede5b248b375c75c10a725a5780c28
archit31uniyal <archituniyal5@gmail.com>
2024-09-29 15:23:12 -0400

    Added support to fetch reviews from Yelp

--- database.py
+++ database.py
@@ -1,20 +1,28 @@
 import psycopg2
 import requests
+import os
 from datasets import load_dataset
 
+
+conn_str = {
+        'user': os.environ['USERNAME'].lower(),
+        'host': 'localhost',
+        'port': 5432,
+        'database': 'insights'
+    }
+
 # Connect to PostgreSQL database
-conn = psycopg2.connect(
-    dbname="insights",
-    user="archit",
-    password="BuildRag",
-    host="localhost"
-)
+# conn = psycopg2.connect(
+#     dbname="insights",
+#     user="postgres",
+#     password=None,
+#     host="localhost"
+# )
+conn = psycopg2.connect(**conn_str)
 cur = conn.cursor()
 
 reviews = load_dataset("McAuley-Lab/Amazon-Reviews-2023")
 print(len(reviews))
-
-exit()
 
 # Insert data into PostgreSQL
 for review in reviews:
